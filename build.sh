e0ab499bf1d698e120bd0b9fc7cc7ce01c42b427
Jakob Sjudin <jakob.sjudin@gmail.com>
2022-07-11 14:23:42 +0200

    Make build/install scripts work on windows

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 cargo build
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    source venv/bin/activate
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    source venv\\Scripts\\activate
+fi
+
 maturin develop
--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+if ! python3 -m venv venv; then
+    echo python3 did not exist, trying python
 
-python3 -m venv venv
-source venv/bin/activate
+    if ! python -m venv venv; then
+        echo Could not find python or python3, exiting
+        exit 1
+    fi
+fi
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    source venv/bin/activate
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    source venv\\Scripts\\activate
+fi
+
 pip install -r requirements.txt
